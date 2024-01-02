514754428e8a8c153b9ebd1a360f1643b416bfa8
Blank-code-bit <kelvincheah02@gmail.com>
2024-01-02 16:40:14 +0800

    initial

--- src/app/BannerScreen/Banner.js
+++ src/app/BannerScreen/Banner.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./Banner.css";
 import requests from "../../Requests";
-import axios from "../axios";
+import axios from "../../axios";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
